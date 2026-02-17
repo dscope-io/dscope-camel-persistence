3cad787a46c157122826c848340ba00df73d7cd9
Roman <roman.dobrik@exilor.com>
2026-02-16 18:05:38 -0800

    Remove a2a adapter module

--- camel-persistence-a2a-adapter/src/main/java/io/dscope/camel/persistence/a2a/package-info.java
+++
@@ -1,4 +0,0 @@
-/**
- * Phase 1 adapter marker package for A2A persistence integration.
- */
-package io.dscope.camel.persistence.a2a;
--- camel-persistence-agui-adapter/src/main/java/io/dscope/camel/persistence/agui/package-info.java
+++
@@ -1,4 +0,0 @@
-/**
- * Phase 1 adapter marker package for AG-UI persistence integration.
- */
-package io.dscope.camel.persistence.agui;
